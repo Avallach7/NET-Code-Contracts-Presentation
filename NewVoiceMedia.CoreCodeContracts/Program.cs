b5bfc1ba11ca8b586c40a8712c84336e005c88cc
Adam Golebiowski <Avallach7@users.noreply.github.com>
2017-08-15 17:39:59 +0200

    Created empty .NET Core project

---
+++ NewVoiceMedia.CoreCodeContracts/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewVoiceMedia.CoreCodeContracts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
