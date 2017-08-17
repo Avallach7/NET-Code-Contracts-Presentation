b4cce918da1179fc68f0b33af3c3953af57e2bc4
Adam Golebiowski <Avallach7@users.noreply.github.com>
2017-08-17 12:46:34 +0200

    Created new Core project

---
+++ DotNet.Core.Contracts/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNet.Core.Contracts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
