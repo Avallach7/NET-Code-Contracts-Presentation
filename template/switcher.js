564ef9917b32bb7acedb0e5e63566ec3834467d8
Unknown <github@avallach.ovh>
2017-08-27 07:54:06 +0200

    Added peer-js based synchronized scrolling

--- template/switcher.js
+++ template/switcher.js
@@ -1,32 +1,72 @@
 document.app = {};
-
-function setCurrentSlideVisibility(visible) {
-    if (visible)
-        document.app.slides[document.app.activeSlideId].scrollIntoView(true);
-    console.log(`slide ${document.app.activeSlideId+1} shown`);
-}
+document.app.onswitch = function (id) {};
+document.app.peerjs = {};
+document.app.peerjs.serverId = "avallach_ovh_contracts_server";
+document.app.peerjs.apiKey = "lwjd5qra8257b9";
 
 function isScrollingBack(event) {
     var backScrollingKeys = ["ArrowUp", "ArrowLeft", "PageUp"];
     return (event instanceof WheelEvent && event.deltaY < 0) ||
-           (event instanceof KeyboardEvent && backScrollingKeys.indexOf(event.key) >= 0);
+        (event instanceof KeyboardEvent && backScrollingKeys.indexOf(event.key) >= 0);
+}
+
+function setSlide(id) {
+    //    console.log(`setSlide(${id}})`);
+    document.app.activeSlideId = id;
+    document.app.slides[id].scrollIntoView(true);
+    document.app.onswitch(id);
+    //    console.log(`document.app.activeSlideId = ${document.app.activeSlideId}`);
 }
 
 function changeSlide(event) {
-    setCurrentSlideVisibility(false);
-    document.app.activeSlideId += isScrollingBack(event) ? -1 : 1;
-    if (document.app.activeSlideId >= document.app.slides.length)
-        document.app.activeSlideId = 0;
-    else if (document.app.activeSlideId < 0)
-        document.app.activeSlideId = document.app.slides.length-1;
-    setCurrentSlideVisibility(true);
+    var newSlide = document.app.activeSlideId + (isScrollingBack(event) ? -1 : 1);
+    //    console.log(`newSlide = ${newSlide}`);
+    if (newSlide >= document.app.slides.length)
+        newSlide = 0;
+    else if (newSlide < 0)
+        newSlide = document.app.slides.length - 1;
+    setSlide(newSlide);
 }
 
-window.addEventListener("load", function() {
+function setupPeerJs() {
+    if (document.location.href.indexOf("server") >= 0) {
+        var server = new Peer(
+            document.app.peerjs.serverId, {
+                key: document.app.peerjs.apiKey
+            }
+        );
+        var connections = [];
+        server.on('connection', function (connection) {
+            connections.push(connection);
+            connection.on('open', function () {
+                connection.send(document.app.activeSlideId);
+            });
+        });
+
+        document.app.onswitch = function () {
+            connections.forEach(function (connection) {
+                connection.send(document.app.activeSlideId);
+            });
+        };
+    } else {
+        peer = new Peer({
+            key: document.app.peerjs.apiKey
+        });
+        peer.on('open', function (id) {
+            peer
+                .connect(document.app.peerjs.serverId)
+                .on('data', function (data) {
+                    setSlide(data);
+                });
+        });
+    }
+}
+
+window.addEventListener("load", function () {
     document.app.slides = document.getElementsByTagName("section");
     document.app.activeSlideId = 0;
-    setCurrentSlideVisibility(true);
     document.body.addEventListener("click", changeSlide);
     document.body.addEventListener("wheel", changeSlide);
     document.body.addEventListener("keydown", changeSlide);
+    setupPeerJs();
 });
