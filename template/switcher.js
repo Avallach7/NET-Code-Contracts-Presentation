864b340956b3c323b87c88a8c07a5529d2fb558d
Adam Golebiowski <Avallach7@users.noreply.github.com>
2017-08-28 15:00:07 +0200

    Even more debugging

--- template/switcher.js
+++ template/switcher.js
@@ -60,21 +60,43 @@ document.app.peerjs.setup = function() {
                 debug: 3
             }
         );
-        document.app.peerjs.connections = [];
-        document.app.peerjs.server.on('connection', function (connection) {
-            console.log(`document.app.peerjs.server.connection(${connection.peer})`);
-            document.app.peerjs.connections.push(connection);
-            connection.on('open', function () {
-                console.log(`document.app.peerjs.connections[${connection.peer}].open`);
-                console.log(`document.app.peerjs.connections[${connection.peer}].send(${document.app.activeSlideId})`);
-                connection.send(document.app.activeSlideId);
+        document.app.peerjs.peerConnections = [];
+        document.app.peerjs.server.on('connection', function (peerConnection) {
+            console.log(`document.app.peerjs.server.onconnection(${peerConnection.peer})`);
+            document.app.peerjs.peerConnections.push(peerConnection);
+            peerConnection.on('open', function () {
+                console.log(`document.app.peerjs.peerConnections[${peerConnection.peer}].open`);
+                console.log(`document.app.peerjs.peerConnections[${peerConnection.peer}].send(${document.app.activeSlideId})`);
+                peerConnection.send(document.app.activeSlideId);
             });
+            peerConnection.on('data', function (data) {
+                console.log(`document.app.peerjs.peerConnection.ondata(${data})`);
+                document.app.setSlide(data);
+            });
+            peerConnection.on('error', function () {
+                console.log(`document.app.peerjs.peerConnection.onerror()`);
+            });
+            peerConnection.on('close', function () {
+                console.log(`document.app.peerjs.peerConnection.onclose()`);
+            });
+        });
+        document.app.peerjs.server.on('close', function () {
+            console.log(`document.app.peerjs.server.onclose()`);
+        });
+        document.app.peerjs.server.on('disconnected', function () {
+            console.log(`document.app.peerjs.server.ondisconnected()`);
+        });
+        document.app.peerjs.server.on('error', function () {
+            console.log(`document.app.peerjs.server.onerror()`);
+        });
+        document.app.peerjs.server.on('open', function () {
+            console.log(`document.app.peerjs.server.onopen()`);
         });
 
         document.app.onswitch = function(id) {
             console.log(`document.app.onswitch(${id})`);
-            document.app.peerjs.connections.forEach(function (connection) {
-                console.log(`document.app.peerjs.connections[${connection.peer}].send(${document.app.activeSlideId})`);
+            document.app.peerjs.peerConnections.forEach(function (connection) {
+                console.log(`document.app.peerjs.peerConnections[${connection.peer}].send(${document.app.activeSlideId})`);
                 connection.send(id);
             });
         };
@@ -85,13 +107,35 @@ document.app.peerjs.setup = function() {
             debug: 3
         });
         document.app.peerjs.peer.on('open', function (id) {
-            console.log(`document.app.peerjs.peer.open(${id})`);
-            document.app.peerjs.peer
-                .connect(document.app.peerjs.serverId)
-                .on('data', function (data) {
-                    console.log(`document.app.peerjs.peer.data(${data})`);
-                    document.app.setSlide(data);
-                });
+            console.log(`document.app.peerjs.peer.onopen(${id})`);
+            document.app.peerjs.serverConnection = document.app.peerjs.peer.connect(document.app.peerjs.serverId, { reliable: true });
+            console.log(`document.app.peerjs.serverConnection.peer = ${document.app.peerjs.serverConnection.peer}`);
+            console.log(`document.app.peerjs.serverConnection.open = ${document.app.peerjs.serverConnection.open}`);
+            document.app.peerjs.serverConnection.on('data', function (data) {
+                console.log(`document.app.peerjs.serverConnection.ondata(${data})`);
+                document.app.setSlide(data);
+            });
+            document.app.peerjs.serverConnection.on('error', function () {
+                console.log(`document.app.peerjs.serverConnection.onerror()`);
+            });
+            document.app.peerjs.serverConnection.on('open', function () {
+                console.log(`document.app.peerjs.serverConnection.onopen()`);
+            });
+            document.app.peerjs.serverConnection.on('close', function () {
+                console.log(`document.app.peerjs.serverConnection.onclose()`);
+            });
+        });
+        document.app.peerjs.peer.on('close', function () {
+            console.log(`document.app.peerjs.peer.onclose()`);
+        });
+        document.app.peerjs.peer.on('disconnected', function () {
+            console.log(`document.app.peerjs.peer.ondisconnected()`);
+        });
+        document.app.peerjs.peer.on('error', function () {
+            console.log(`document.app.peerjs.peer.onerror()`);
+        });
+        document.app.peerjs.peer.on('connection', function () {
+            console.log(`document.app.peerjs.peer.onconnection()`);
         });
     }
 }
