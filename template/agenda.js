c2f5385bd2185eb461379dd999a1759dabb82fd6
Adam Golebiowski <Avallach7@users.noreply.github.com>
2017-09-04 13:57:20 +0200

    Agenda items are now hyperlinks

--- template/agenda.js
+++ template/agenda.js
@@ -1,8 +1,8 @@
 window.addEventListener("load", function () {
-    var slides = document.getElementsByTagName("section");
+    //var slides = document.getElementsByTagName("section");
     [].forEach.call(document.getElementsByClassName("agenda"), function (agenda) {
         function getTitle(slideIndex) {
-            var slide = slides[slideIndex];
+            var slide = document.app.slides[slideIndex];
             if (slide.dataset.title != undefined && !slide.dataset.title.endsWith(" cont."))
                 return (slideIndex + 1) + ". " + slide.dataset.title;
             else {
@@ -11,14 +11,16 @@ window.addEventListener("load", function () {
         }
         var list = document.createElement("ol");
         agenda.appendChild(list);
-        [].forEach.call(slides, function (slide, index) {
+        [].forEach.call(document.app.slides, function (slide, index) {
             if (slide.classList.contains("agenda") ||
+                slide.classList.contains("title") ||
                 slide.childElementCount == 0)
                 return;
             var title = getTitle(index)
             if (title == undefined)
                 return;
-            var entry = document.createElement("li");
+            var entry = document.createElement("a");
+            entry.href = "javascript:document.app.slides[" + index + "].scrollIntoView(true)";
             entry.textContent = title;
             list.appendChild(entry);
         });
